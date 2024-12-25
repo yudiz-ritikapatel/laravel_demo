ef3801e54e6e557c9cbfe79dd5cf9fa3ddf8596d
yudiz-ritikapatel <ritika.patel@yudiz.com>
2024-12-25 10:11:50 +0530

    added livewire crud

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'body'
+    ];
+}
