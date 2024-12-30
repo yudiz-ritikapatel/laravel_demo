5b7a6d28e5a033b67ddda67040215a40e060dd9a
yudiz-ritikapatel <ritika.patel@yudiz.com>
2024-12-30 15:06:23 +0530

    spartie added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) { 
         $middleware->alias([
             'checkStatus' => \App\Http\Middleware\CheckStatus::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
